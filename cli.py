829dee82fa089f8b3e79be61d114219ac26964e8
Brendon Hollingsworth <tucaeg@yahoo.com>
2015-11-28 19:40:58 -0800

    Mostly finished. Got get, ls and quit working

--- cli.py
+++ cli.py
@@ -1,136 +1,183 @@
-__author__ = 'BrendonH'
+# Group Member: Brendon Hollingsworth, Jarid Goodwin, Jacob Goodwin
 
 import sys
 import socket
+# ************************************************
+# Receives the specified number of bytes
+# from the specified socket
+# @param sock - the socket from which to receive
+# @param numBytes - the number of bytes to receive
+# @return - the bytes received
+# *************************************************
+def recvAll(sock, numBytes):
+	# The buffer
+	recvBuff = ""
+
+	# The temporary buffer
+	tmpBuff = ""
+
+	# Keep receiving till all is received
+	while len(recvBuff) < numBytes:
+
+		# Attempt to receive bytes
+		tmpBuff =  sock.recv(numBytes).decode('ascii')  #had to add.decode for it to work
+
+		# The other side has closed the socket
+		if not tmpBuff:
+			break
+
+
+
+		# Add the received bytes to the buffer
+		recvBuff += tmpBuff
+	return recvBuff
 
 #from ephemeral.py, not entirely sure how to use it yet
-def getEphemeralPort():
-    # Create a socket
-    welcomeSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def getEphemeralSock():
+	# Create a socket
+	welcomeSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Bind the socket to port 0
-    welcomeSocket.bind(('',0))
+	# Bind the socket to port 0
+	welcomeSocket.bind(('',0))
 
-    # Retreive the ephemeral port number
-    print("I chose ephemeral port: ", welcomeSocket.getsockname()[1])
+	# Retreive the ephemeral port number
+	print("I chose ephemeral port: ", welcomeSocket.getsockname()[1])
 
-    return welcomeSocket
+	return welcomeSocket
 
 
 
 def put(dataSock, fileName):
-    #need to check if file name is valid
-
-    # The name of the file
-    # fileName = "file.txt"
-
-    # Open the file
-    fileObj = open(fileName, "r")
-
-
-
-    # The number of bytes sent
-    numSent = 0
-
-    # The file data
-    fileData = None
-
-    # Keep sending until all is sent
-    while True:
-
-        # Read 65536 bytes of data
-        fileData = fileObj.read(65536)
-
-        # Make sure we did not hit EOF
-        if fileData:
-
-
-            # Get the size of the data read
-            # and convert it to string
-            dataSizeStr = str(len(fileData))
-
-            # Prepend 0's to the size string
-            # until the size is 10 bytes
-            while len(dataSizeStr) < 10:
-                dataSizeStr = "0" + dataSizeStr
+	#need to check if file name is valid
+	# Open the file
+	fileObj = open(fileName, "r")
+	# The number of bytes sent
+	numSent = 0
+	# The file data
+	fileData = None
+	# Keep sending until all is sent
+	while True:
+		# Read 65536 bytes of data
+		fileData = fileObj.read(65536)
+		# Make sure we did not hit EOF
+		if fileData:
+			# Get the size of the data read
+			# and convert it to string
+			dataSizeStr = str(len(fileData))
+			# Prepend 0's to the size string
+			# until the size is 10 bytes
+			while len(dataSizeStr) < 10:
+				dataSizeStr = "0" + dataSizeStr
+			# Prepend the size of the data to the
+			# file data.
+			fileData = dataSizeStr + fileData
+			# The number of bytes sent
+			numSent = 0
+			# Send the data!
+			while len(fileData) > numSent:
+				numSent += dataSock.send(fileData.encode('ascii'))
+		# The file has been read. We are done
+		else:
+			break
 
 
-            # Prepend the size of the data to the
-            # file data.
-            fileData = dataSizeStr + fileData
+	print("Sent ", numSent, " bytes.")
 
-            # The number of bytes sent
-            numSent = 0
+	# Close the socket and the file
+	dataSock.close()
+	fileObj.close()
 
-            # Send the data!
-            while len(fileData) > numSent:
-                numSent += dataSock.send(fileData.encode('ascii'))    #this is where it breaks right now
+def get(dataSock, fileName):
+	fileData = ""
+	recvBuff = ""
+	fileSize = 0
+	fileSizeBuff = ""
+	fileSizeBuff = recvAll(dataSock, 10)
+	fileSize = int(fileSizeBuff)
+	fileData = recvAll(dataSock, fileSize)
+	print("The file data is: ")
+	print(fileData)
+	dataSock.close()
 
-        # The file has been read. We are done
-        else:
-            break
-
-
-    print("Sent ", numSent, " bytes.")
-
-    # Close the socket and the file
-    dataSock.close()
-    fileObj.close()
+def ls(dataSock):
+	fileData = ""
+	recvBuff = ""
+	fileSize = 0
+	fileSizeBuff = ""
+	fileSizeBuff = recvAll(dataSock, 10)
+	fileSize = int(fileSizeBuff)
+	fileData = recvAll(dataSock, fileSize)
+	print("The file data is: ")
+	print(fileData)
+	dataSock.close()
 
 def sendCommand(commandString, connSock):
-    # send command
-    commandString = str(len(commandString)) + " " + commandString   #prepend size of command string
-    bytes_sent = 0
-    while bytes_sent < len(commandString):
-        bytes_sent += connSock.send(commandString.encode('ascii'))
+	# send command
+	size = str(len(commandString))
+	# Prepend 0's to the size string
+	# until the size is 2 bytes
+	while len(size) < 2:
+		size = "0" + size
+	commandString = size + " " + commandString   #prepend size of command string
+	bytes_sent = 0
+	while bytes_sent < len(commandString):
+		bytes_sent += connSock.send(commandString.encode('ascii'))
 
 
 
 def main(argv):
 
-    # Command line checks
-    if len(argv) < 2:
-        print("USAGE python " + argv[0] + " <FILE NAME>")
+	# Command line checks
+	if len(argv) < 2:
+		print("USAGE python " + argv[0] + " <FILE NAME>")
 
-    while True:
-
-
-        # Server address
-        serverAddr = argv[1]
-
-        # Server port
-        serverPort = int(argv[2])
-
-        # Create a TCP socket
-        connSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # Connect to the server
-        connSock.connect((serverAddr, serverPort))
-
-        commandString = input("ftp> ")
-        command = commandString.split()   #splits string into list of words
+	while True:
 
 
+		# Server address
+		serverAddr = argv[1]
 
-        if(command[0].lower() == "get"):
-            pass    #pass is a placeholder for the eventual function get() function that will be called
-        elif(command[0].lower() == "put"):
-            welcomeSock = getEphemeralPort()
+		# Server port
+		serverPort = int(argv[2])
 
-            commandString = commandString + " " + str(welcomeSock.getsockname()[1])
+		# Create a TCP socket
+		connSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		# Connect to the server
+		connSock.connect((serverAddr, serverPort))
 
-            welcomeSock.listen(1)
-            sendCommand(commandString, connSock)
-            dataSock, addr = welcomeSock.accept()
-            put(dataSock, command[1])
-        elif(command[0].lower() == "ls"):
-            pass
-        elif(command[0].lower() == "quit"):
-            #function for sending quit goes here
-            sys.exit(0)
-        else:
-            print("Invalid command")
+		commandString = input("ftp> ")
+		command = commandString.split()   #splits string into list of words
+
+
+
+		if(command[0].lower() == "get"):
+			welcomeSock = getEphemeralSock()
+			commandString = commandString + " " + str(welcomeSock.getsockname()[1])
+			welcomeSock.listen(1)
+			sendCommand(commandString, connSock)
+			dataSock, addr = welcomeSock.accept()
+			get(dataSock, command[1])
+		elif(command[0].lower() == "put"):
+			welcomeSock = getEphemeralSock()
+			commandString = commandString + " " + str(welcomeSock.getsockname()[1])
+			welcomeSock.listen(1)
+			sendCommand(commandString, connSock)
+			dataSock, addr = welcomeSock.accept()
+			put(dataSock, command[1])
+		elif(command[0].lower() == "ls"):
+			welcomeSock = getEphemeralSock()
+			commandString = commandString + " " + str(welcomeSock.getsockname()[1])
+			welcomeSock.listen(1)
+			sendCommand(commandString, connSock)
+			dataSock, addr = welcomeSock.accept()
+			ls(dataSock)
+		elif(command[0].lower() == "quit"):
+			sendCommand(commandString, connSock)
+			sys.exit(1)
+		else:
+			print("Invalid command")
 
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+	main(sys.argv)
--- serv.py
+++ serv.py
@@ -1,7 +1,8 @@
-__author__ = 'BrendonH'
+# Group Members: Brendon Hollingsworth, Jarid Goodwin, Jacob Goodwin
+
 import sys
 import socket
-
+import subprocess
 # ************************************************
 # Receives the specified number of bytes
 # from the specified socket
@@ -48,23 +49,68 @@ def put(fileName, dataSock):
 	fileSizeBuff = ""
 	# Receive the first 10 bytes indicating the
 	# size of the file
-	fileSizeBuff = recvAll(dataSock, 10)	#This is where it breaks for this file
+	fileSizeBuff = recvAll(dataSock, 10)
 	# Get the file size
 	fileSize = int(fileSizeBuff)
 	print("The file size is ", fileSize)
 	# Get the file data
 	fileData = recvAll(dataSock, fileSize)
+	print(fileName,"received.")
 	print("The file data is: ")
 	print(fileData)
+
+def get(fileName, dataSock):
+	fileObj = open(fileName, "r")
+	numSent = 0
+	fileData = None
+	while True:
+		fileData = fileObj.read(65536)
+		if fileData:
+			dataSizeStr = str(len(fileData))
+			while len(dataSizeStr) < 10:
+				dataSizeStr = "0" + dataSizeStr
+			fileData = dataSizeStr + fileData
+			numSent = 0
+			while len(fileData) > numSent:
+				numSent += dataSock.send(fileData.encode('ascii'))
+		else:
+			break
+	print(fileName,"sent.")
+	print("Sent", numSent, " bytes.")
+	dataSock.close()
+	fileObj.close()
+
+def ls(dataSock):
+	lsData = ""
+	#if windows use dir instead of ls -l
+	if sys.platform == "win32":
+		for line in subprocess.getstatusoutput('dir'):
+			lsData += str(line)
+	else:
+		for line in subprocess.getstatusoutput('ls -l'):
+			lsData += str(line)
+
+	dataSizeStr = str(len(lsData))
+	# Prepend 0's to the size string
+	# until the size is 10 bytes
+	while len(dataSizeStr) < 10:
+		dataSizeStr = "0" + dataSizeStr
+	# Prepend the size of the data to the
+	# file data.
+	lsData = dataSizeStr + lsData
+	# The number of bytes sent
+	numSent = 0
+	# Send the data!
+	while len(lsData) > numSent:
+		numSent += dataSock.send(lsData.encode('ascii'))
+	print("Data sent.")
 
 def getCommand(sock):
 	commandBytes = int(recvAll(sock,3))	#receive 1st 3 bytes (2-byte number and a space) indicating command size
 	return recvAll(sock, commandBytes)	#receive the remaining command
 
-
 def main(argv):
 	if len(argv) > 1:
-#         from sendfileserv.py
 		# Accept connections forever
 		# The port on which to listen
 		listenPort = int(argv[1])
@@ -78,7 +124,6 @@ def main(argv):
 		# Start listening on the socket
 		welcomeSock.listen(1)
 		while True:
-
 			print ("Waiting for connections...")
 
 			# Accept connections
@@ -90,21 +135,24 @@ def main(argv):
 			clientCommand = clientCommand.split()
 
 			if(clientCommand[0].lower() == "get"):
-				pass    #pass is a placeholder for the eventual function get() function that will be called
-			elif(clientCommand[0].lower() == "put"):
-				# connect to datasocket
+				print("Received get command. Preparing to send file.")
 				dataSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-				dataSock.connect((addr[0], int(clientCommand[2])))	#addr[0] is the address the last socket connected to, clientCommand[2] is the ephemeral port sent by client
+				dataSock.connect((addr[0], int(clientCommand[-1])))	#addr[0] is the address the last socket connected to, clientCommand[2] is the ephemeral port sent by client
+				get(clientCommand[1], dataSock)
+			elif(clientCommand[0].lower() == "put"):
+				print("Received put command. Preparing to receive file.")
+				dataSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				dataSock.connect((addr[0], int(clientCommand[-1])))	#addr[0] is the address the last socket connected to, clientCommand[2] is the ephemeral port sent by client
 				put(clientCommand[1], dataSock)
 			elif(clientCommand[0].lower() == "ls"):
-				pass
+				print("Received ls command. Preparing to send folder contents.")
+				dataSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				dataSock.connect((addr[0], int(clientCommand[-1])))	#addr[0] is the address the last socket connected to, clientCommand[2] is the ephemeral port sent by client
+				ls(dataSock)
 			elif(clientCommand[0].lower() == "quit"):
-				#function for sending quit goes here
-				sys.exit(0)
-			# put(clientSock)
+				print(addr," has closed their session")
 
-			# Close our side
-			clientSock.close()
+
 
 
 if __name__ == "__main__":
